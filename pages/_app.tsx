bed47a8289cf45c62ef36f7fc087bbf05c87f6fe
dwaynediddy <burrows.dwayne@icloud.com>
2022-11-01 12:56:38 +0100

    added layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
-import '../styles/globals.css'
+import Router from 'next/router'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import NProgress from 'nprogress'
+
+import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   )
 }
